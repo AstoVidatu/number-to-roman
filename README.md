490890c3ca4a554a32836032164f19a257022ef0
Stefan Jankovic <freelance.stefan@gmail.com>
2018-04-06 20:51:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# number-to-roman
+
+A function that creates a Roman number from an normal input
+	
+	Example:
+	Input:  436
+	Output: CDXXXVI
+
+~AstoVidatu
---
+++ roman.js
@@ -0,0 +1,24 @@
+// 	Number to Roman
+// 	A function that creates a Roman number from an normal input
+// 
+// 	Example:
+// 	Input:  436
+// 	Output: CDXXXVI
+// 
+// 	~AstoVidatu
+
+var roman = {M:1000, CM:900, D:500, CD:400, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1};
+var number = 1;
+var result = "";
+
+function convertRoman(n) {
+	for( i in roman){
+		while(n >= roman[i]){
+			result += i;
+			n -= roman[i];
+		}
+	}
+	console.log(result);
+}
+
+convertRoman(number);
