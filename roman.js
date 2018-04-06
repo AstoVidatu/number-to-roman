369341a089ab83e6a7b18f270dfe61ae85c303f9
Stefan Jankovic <freelance.stefan@gmail.com>
2018-04-06 23:41:12 +0200

    Update

--- roman.js
+++ roman.js
@@ -8,17 +8,19 @@
 // 	~AstoVidatu
 
 var roman = {M:1000, CM:900, D:500, CD:400, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1};
-var number = 1;
+var number = 436;
 var result = "";
 
 function convertRoman(n) {
 	for( i in roman){
 		while(n >= roman[i]){
 			result += i;
+			console.log('n: 	 ', n);
 			n -= roman[i];
+			console.log('result: ', result);
+
 		}
 	}
 	console.log(result);
 }
 
-convertRoman(number);
